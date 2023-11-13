14c8e667ad594af29fa238ec12681ab30d45603f
UserNanno <juan.canecillas@unmsm.edu.pe>
2023-11-13 17:14:21 -0500

    update .sql

--- SGH-FISI/src/db/sgh-fisi.sql
+++ SGH-FISI/src/db/sgh-fisi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-11-2023 a las 00:09:16
+-- Tiempo de generación: 13-11-2023 a las 23:13:53
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -29,17 +29,25 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `ciclos` (
   `id_ciclo` int(11) NOT NULL,
-  `nombre` varchar(100) NOT NULL
+  `ciclo` varchar(20) NOT NULL,
+  `id_semestre` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `ciclos`
 --
 
-INSERT INTO `ciclos` (`id_ciclo`, `nombre`) VALUES
-(1, 'Ciclo I'),
-(2, 'Ciclo II'),
-(3, 'Ciclo III');
+INSERT INTO `ciclos` (`id_ciclo`, `ciclo`, `id_semestre`) VALUES
+(1, '1', NULL),
+(2, '2', NULL),
+(3, '3', NULL),
+(6, '4', NULL),
+(7, '5', NULL),
+(8, '6', NULL),
+(9, '7', NULL),
+(10, '8', NULL),
+(11, '9', NULL),
+(12, '10', NULL);
 
 -- --------------------------------------------------------
 
@@ -51,10 +59,12 @@ CREATE TABLE `cursos` (
   `id_curso` int(11) NOT NULL,
   `codigo` varchar(20) NOT NULL,
   `nombre` varchar(100) NOT NULL,
+  `creditos` decimal(6,1) NOT NULL,
   `cantidad_alumnos` int(11) DEFAULT NULL,
+  `id_semestre` int(11) DEFAULT NULL,
+  `id_ciclo` int(11) DEFAULT NULL,
   `id_plan_estudios` int(11) DEFAULT NULL,
   `id_seccion` int(11) DEFAULT NULL,
-  `id_ciclo` int(11) DEFAULT NULL,
   `id_horario` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -62,12 +72,13 @@ CREATE TABLE `cursos` (
 -- Volcado de datos para la tabla `cursos`
 --
 
-INSERT INTO `cursos` (`id_curso`, `codigo`, `nombre`, `cantidad_alumnos`, `id_plan_estudios`, `id_seccion`, `id_ciclo`, `id_horario`) VALUES
-(1, 'CURSO001', 'Curso de ejemplo', 50, 1, 1, 1, 5),
-(2, 'CURSO002', 'Otro curso', 40, 2, 2, 2, 7),
-(3, 'CURSO003', 'Tercer curso', 60, 3, 3, 3, 6),
-(4, 'CURSO004', 'SISTEMAS OPERATIVOS', 40, 1, 1, 1, 1),
-(6, 'CURSO005', 'Estructura de Datos', 60, 1, NULL, 1, 3);
+INSERT INTO `cursos` (`id_curso`, `codigo`, `nombre`, `creditos`, `cantidad_alumnos`, `id_semestre`, `id_ciclo`, `id_plan_estudios`, `id_seccion`, `id_horario`) VALUES
+(1, 'CURSO001', 'Curso de ejemplo', 0.0, 50, NULL, 1, 1, NULL, 5),
+(2, 'CURSO002', 'Otro curso', 0.0, 40, NULL, 2, 2, 2, 7),
+(3, 'CURSO003', 'Tercer curso', 0.0, 60, NULL, 3, 3, 3, 6),
+(4, 'CURSO004', 'SISTEMAS OPERATIVOS', 0.0, 40, NULL, 1, 1, 1, 1),
+(6, 'CURSO005', 'Estructura de Datos', 0.0, 60, NULL, 1, 1, 2, 3),
+(7, 'CURSO005', 'Estructura de Datos', 0.0, 40, NULL, 1, 1, 2, NULL);
 
 -- --------------------------------------------------------
 
@@ -91,7 +102,7 @@ INSERT INTO `horarios` (`id_horario`, `hora`) VALUES
 (4, '2pm - 4pm'),
 (5, '4pm - 6pm'),
 (6, '6pm - 8pm'),
-(7, '8pm - 10p´m');
+(7, '8pm - 10pm');
 
 -- --------------------------------------------------------
 
@@ -109,9 +120,9 @@ CREATE TABLE `plan_estudios` (
 --
 
 INSERT INTO `plan_estudios` (`id_plan_estudios`, `nombre`) VALUES
-(1, 'Plan A'),
-(2, 'Plan B'),
-(3, 'Plan C');
+(1, 'Plan 2015'),
+(2, 'Plan 2018'),
+(3, 'Plan 2023');
 
 -- --------------------------------------------------------
 
@@ -156,6 +167,28 @@ INSERT INTO `seccion` (`id_seccion`, `nombre`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `semestre`
+--
+
+CREATE TABLE `semestre` (
+  `id_semestre` int(11) NOT NULL,
+  `anio` int(11) DEFAULT NULL,
+  `ciclo_academico` varchar(10) DEFAULT NULL,
+  `estado` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `semestre`
+--
+
+INSERT INTO `semestre` (`id_semestre`, `anio`, `ciclo_academico`, `estado`) VALUES
+(17, 2025, '1', ''),
+(18, 2026, '0', ''),
+(19, 2023, '4', '');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
@@ -171,7 +204,9 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id_usuario`, `nombre_usuario`, `password`, `id_rol`) VALUES
-(1, 'xmldoot', 'xmldoot', 1);
+(1, 'xmlroot', 'xmlroot', 1),
+(2, 'xmlmood', 'xmlmood', 3),
+(3, 'xmluser', 'xmluser', 2);
 
 --
 -- Índices para tablas volcadas
@@ -181,7 +216,8 @@ INSERT INTO `usuarios` (`id_usuario`, `nombre_usuario`, `password`, `id_rol`) VA
 -- Indices de la tabla `ciclos`
 --
 ALTER TABLE `ciclos`
-  ADD PRIMARY KEY (`id_ciclo`);
+  ADD PRIMARY KEY (`id_ciclo`),
+  ADD KEY `id_semestre` (`id_semestre`);
 
 --
 -- Indices de la tabla `cursos`
@@ -191,7 +227,8 @@ ALTER TABLE `cursos`
   ADD KEY `id_plan_estudios` (`id_plan_estudios`),
   ADD KEY `id_seccion` (`id_seccion`),
   ADD KEY `id_ciclo` (`id_ciclo`),
-  ADD KEY `id_horario` (`id_horario`);
+  ADD KEY `id_horario` (`id_horario`),
+  ADD KEY `id_semestre` (`id_semestre`);
 
 --
 -- Indices de la tabla `horarios`
@@ -218,6 +255,12 @@ ALTER TABLE `seccion`
   ADD PRIMARY KEY (`id_seccion`);
 
 --
+-- Indices de la tabla `semestre`
+--
+ALTER TABLE `semestre`
+  ADD PRIMARY KEY (`id_semestre`);
+
+--
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
@@ -232,13 +275,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `ciclos`
 --
 ALTER TABLE `ciclos`
-  MODIFY `id_ciclo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_ciclo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `cursos`
 --
 ALTER TABLE `cursos`
-  MODIFY `id_curso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_curso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `horarios`
@@ -265,14 +308,26 @@ ALTER TABLE `seccion`
   MODIFY `id_seccion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT de la tabla `semestre`
+--
+ALTER TABLE `semestre`
+  MODIFY `id_semestre` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `ciclos`
+--
+ALTER TABLE `ciclos`
+  ADD CONSTRAINT `ciclos_ibfk_1` FOREIGN KEY (`id_semestre`) REFERENCES `semestre` (`id_semestre`);
 
 --
 -- Filtros para la tabla `cursos`
@@ -281,7 +336,8 @@ ALTER TABLE `cursos`
   ADD CONSTRAINT `cursos_ibfk_1` FOREIGN KEY (`id_plan_estudios`) REFERENCES `plan_estudios` (`id_plan_estudios`),
   ADD CONSTRAINT `cursos_ibfk_2` FOREIGN KEY (`id_seccion`) REFERENCES `seccion` (`id_seccion`),
   ADD CONSTRAINT `cursos_ibfk_3` FOREIGN KEY (`id_ciclo`) REFERENCES `ciclos` (`id_ciclo`),
-  ADD CONSTRAINT `cursos_ibfk_4` FOREIGN KEY (`id_horario`) REFERENCES `horarios` (`id_horario`);
+  ADD CONSTRAINT `cursos_ibfk_4` FOREIGN KEY (`id_horario`) REFERENCES `horarios` (`id_horario`),
+  ADD CONSTRAINT `cursos_ibfk_5` FOREIGN KEY (`id_semestre`) REFERENCES `semestre` (`id_semestre`);
 
 --
 -- Filtros para la tabla `usuarios`
